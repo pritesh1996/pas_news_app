63d143291d46051ea172b6b646ceda56af344346
Pritesh Kumbhare <priteshkumbhare@gmail.com>
2016-10-13 15:34:33 +0530

    Added goodckeditor in add_form

--- news/views.py
+++ news/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from .models import News
 from django.http import HttpResponse
+from django.db import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
     return render(request,'home.html')
 
 def news(request):
-    all_news = News.objects.all()
-    return render(request, "news.html", {'all_news': all_news})
+    all_news_full = News.objects.all().order_by('datetime')
+    paginator = Paginator(all_news_full, 4)
+    page = request.GET.get('page')
+    try:
+        all_news = paginator.page(page)
+    except PageNotAnInteger:
+        all_news = paginator.page(1)
+    except EmptyPage:
+        all_news = paginator.page(paginator.num_pages)
+    return render(request, "news.html", {'all_news': all_news , 'range': paginator.page_range })
 
 def add_news(request):
     return render(request,'add_news.html')
